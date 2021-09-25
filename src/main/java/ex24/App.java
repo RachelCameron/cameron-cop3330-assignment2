f39da96cc764e286f38d1a178a10b49914c58794
Rachel Cameron <rachleecameron@gmail.com>
2021-09-25 15:23:29 -0400

    ex24

--- src/main/java/ex24/App.java
+++ src/main/java/ex24/App.java
@@ -6,9 +6,44 @@ package ex24;
  */
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:");
+        System.out.printf("Enter the first string:");
+        String first = input.nextLine();
+        System.out.printf("Enter the second string:");
+        String second = input.nextLine();
+
+        if (isAnagram(first, second))
+        {
+            System.out.println("\"" + first + "\" and " + "\"" + second + "\" are anagrams.");
+        }
+        else
+            System.out.println("\"" + first + "\" and " + "\"" + second + "\" are not anagrams.");
+        }
+
+    public static boolean isAnagram(String first, String second) {
+        if (first.length() != second.length()) {
+            return false;
+        }
+        else
+        {
+            char [] firstArray = first.toCharArray();
+            char [] secondArray = second.toCharArray();
+
+            Arrays.sort(firstArray);
+            Arrays.sort(secondArray);
+
+            if (Arrays.equals(firstArray, secondArray)) {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
