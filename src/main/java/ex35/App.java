6f3fb0aa743705750986fa98c464b706ee34e976
Rachel Cameron <rachleecameron@gmail.com>
2021-09-27 16:03:19 -0400

    ex35

--- src/main/java/ex35/App.java
+++ src/main/java/ex35/App.java
@@ -5,10 +5,35 @@ package ex35;
  *  Copyright 2021 Rachel Cameron
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+        String name;
+        boolean empty = false;
+
+        while(!empty){
+            System.out.println("Enter a name: ");
+            name = input.nextLine();
+            if(name.equals("")){
+                empty = true;
+            }
+            else{
+                names.add(name);
+            }
+        }
+
+        System.out.println("The winner is " + randomizer(names) + "!");
+
+    }
+    public static String randomizer(ArrayList<String> entries){
+        Random random = new Random();
+        int winningIndex = random.nextInt(entries.size());
+        return entries.get(winningIndex);
     }
 }
