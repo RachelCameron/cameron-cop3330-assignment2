3d535e62aefc65b26b3dcbcd0fede1ab764c1f16
Rachel Cameron <rachleecameron@gmail.com>
2021-09-26 20:35:34 -0400

    ex30

--- src/main/java/ex30/App.java
+++ src/main/java/ex30/App.java
@@ -5,10 +5,16 @@ package ex30;
  *  Copyright 2021 Rachel Cameron
  */
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        multiplicationTable(); }
+
+    private static void multiplicationTable() {
+     for (int i = 1; i <= 12; i++) {
+            for (int j = 1; j <= 12; j++) {
+             System.out.printf("%4d", (i * j));
+        }
+             System.out.print("\n");
     }
+}
 }
