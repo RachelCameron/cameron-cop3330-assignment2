3b6d159396c6daaf74e45589b834acfb4bf3fe95
Rachel Cameron <rachleecameron@gmail.com>
2021-09-26 20:55:55 -0400

    ex30 test

---
+++ src/test/java/ex30/AppTest.java
@@ -0,0 +1,18 @@
+package ex30;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void multiplicationTableTrue() {
+        App AppTest = new App();
+        Assertions.assertEquals(1, AppTest.multiplicationTable());
+    }
+}
