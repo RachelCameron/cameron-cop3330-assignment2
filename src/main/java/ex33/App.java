33c3425d44fe644950cc5d1511ee7014ec99b5f7
Rachel Cameron <rachleecameron@gmail.com>
2021-09-26 22:59:11 -0400

    ex33

--- src/main/java/ex33/App.java
+++ src/main/java/ex33/App.java
@@ -6,9 +6,25 @@ package ex33;
  */
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("What's  your question?\n> ");
+        String question = input.nextLine();
+        Random random = new Random();
+        int randomizer = random.nextInt(4);
+        System.out.println(result(randomizer));
+    }
+
+    public static String result(int answer) {
+        ArrayList <String> list = new ArrayList <String>();
+        list.add("Yes.");
+        list.add("No.");
+        list.add("Maybe.");
+        list.add("Ask again later.");
+        return list.get(answer);
     }
 }
