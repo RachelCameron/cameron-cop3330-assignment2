aca2ad9648380ce14805bc063633cd14e25d3a3c
Rachel Cameron <rachleecameron@gmail.com>
2021-09-27 23:51:09 -0400

    ex32 test

--- src/test/java/ex32/AppTest.java
+++ src/test/java/ex32/AppTest.java
@@ -1,7 +1,15 @@
 package ex32;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
-    //ran out of time on
+    @Test
+    void getRandomNumber(){
+        App AppTest = new App();
+        int num = AppTest.getRandomNumber(1);
+        Assertions.assertTrue(num < 10 && num > 1);
+        int num2 = AppTest.getRandomNumber(5);
+        Assertions.assertTrue(num2 < 100000 && num2 > 1);
+    }
 }
