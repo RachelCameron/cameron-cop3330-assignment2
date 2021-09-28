d02d90e380425e9831d64cf40ebc8d6ef33ce562
Rachel Cameron <rachleecameron@gmail.com>
2021-09-27 22:49:57 -0400

    ex29 test

--- src/test/java/ex29/AppTest.java
+++ src/test/java/ex29/AppTest.java
@@ -1,4 +1,16 @@
 package ex29;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
+    @Test
+    public void getYears() {
+        App AppTest = new App();
+        Assertions.assertEquals(18, AppTest.getYears(4));
+    }
 }
