921e11e259ca072befb642f31fd22361441cc15f
Rachel Cameron <rachleecameron@gmail.com>
2021-09-27 20:24:30 -0400

    ex39

--- src/main/java/ex39/App.java
+++ src/main/java/ex39/App.java
@@ -5,10 +5,54 @@ package ex39;
  *  Copyright 2021 Rachel Cameron
  */
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        List<Map<String, String>> employees = readEmployeesFromFile();
+        sortEmployees(employees);
+        printTable(employees);
+    }
+
+    private static List<Map<String, String>> readEmployeesFromFile() {
+        List<Map<String, String>> result = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(new File("employees.txt"));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] data = line.split(";");
+
+                Map<String, String> employee = new HashMap<>();
+                employee.put("first name", data[0]);
+                employee.put("last name", data[1]);
+                employee.put("position", data[2]);
+                employee.put("separation date", data[3]);
+
+                result.add(employee);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: Could not find employees.txt file!");
+        }
+
+        return result;
+    }
+
+    public static void sortEmployees(List<Map<String, String>> employees) {
+
+    }
+
+    public static void printTable(List<Map<String, String>> employees) {
+        System.out.format("%-15s| %-15s| %-15s%n", "Name", "Position",
+                "Separation Date");
+        System.out.println("---------------------------------------------");
+        for (Map<String, String> e : employees) {
+            System.out.format("%-15s| %-15s| %-15s%n", e.get("first name"),
+                    e.get("last name"), e.get("separation date"));
+        }
     }
 }
