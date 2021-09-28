fc756e6eff0a6a5e0bc4a4ade68ae50c31121e6d
Rachel Cameron <rachleecameron@gmail.com>
2021-09-27 22:49:47 -0400

    ex29

--- src/main/java/ex29/App.java
+++ src/main/java/ex29/App.java
@@ -10,5 +10,33 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int years;
+        int rate;
+        boolean validEntry = false;
+        System.out.println("What is the rate of return?");
+        String entry = input.nextLine();
+        do{
+            if(entry.matches(".*[a-z].*")){
+                System.out.println("Error: Invalid Input");
+                System.out.println("What is the rate of return?");
+                entry = input.nextLine();
+            }
+            else {
+                rate = Integer.parseInt(entry);
+                if(rate == 0){
+                    System.out.println("Error: Invalid Input");
+                    System.out.println("What is the rate of return?");
+                    entry = input.nextLine();
+                }
+                else{
+                    years = getYears(rate);
+                    System.out.println("It will take " + years + " years to double your investment.");
+                    validEntry = true;
+                }
+            }
+        }while(!validEntry);
+    }
+    public static int getYears(int rate){
+        return 72/rate;
     }
 }
